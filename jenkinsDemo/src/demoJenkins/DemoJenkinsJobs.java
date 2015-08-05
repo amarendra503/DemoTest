2e7cbf7b9ffa5cb57ddf689fe84e0b028329f25b
amarendrakumar503@gmail.com <amarendrakumar503@gmail.com>
2015-08-05 18:52:47 +0530

    initial checkin of jenkins Demo.

---
+++ jenkinsDemo/src/demoJenkins/DemoJenkinsJobs.java
@@ -0,0 +1,12 @@
+package demoJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJobs {
+	@Test
+	public void HelloJenkins(){
+		System.out.println("Welcome to Jenkins World");
+		
+	}
+
+}
